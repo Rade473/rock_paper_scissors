8ee3edf4b7ebd99f8b393f772b1ac211047bde44
Rade473 <r.shekularats@gmail.com>
2022-04-25 15:57:35 +0200

    Added score count and a little styling

--- js/javascript.js
+++ js/javascript.js
@@ -2,6 +2,9 @@ const choices = ['Rock','Paper','Scissors']
 rock = choices [0]
 paper = choices [1]
 scissors = choices [2]
+let tiecount=0;
+let wincount=0;
+let losecount=0;
 function computerPlay() {
  random = Math.floor(Math.random()*choices.length)
  return computerChoice = choices[random]
@@ -9,6 +12,7 @@ function computerPlay() {
 function playerChoose(x) {
    if (x===rock) {
        return playerChoice = rock;
+       
    }
  else if (x===paper){
         return playerChoice = paper;
@@ -16,28 +20,66 @@ function playerChoose(x) {
 else if (x===scissors){
     return playerChoice = scissors;
 }
+
 }
 function game (){
+    updateScore();
     computerPlay();
     if (playerChoice===computerChoice) {
-        alert ('Its a tie!')
+        tiecount++;
+        document.getElementById('result').textContent = 'It\'s a tie!'
+        document.getElementById('result').style.backgroundColor = 'yellow'
+        document.getElementById('result').style.color = 'black'
     }
     else if (playerChoice === rock && computerChoice === paper){
-        alert ("You loose!")
+        document.getElementById('result').textContent = 'You Lost!'
+        losecount++;
+        document.getElementById('result').style.backgroundColor = 'red'
+        document.getElementById('result').style.color = 'white'
     }
     else if (playerChoice === paper && computerChoice === rock){
-        alert ("You win!")
+        document.getElementById('result').textContent = 'You WON!'
+        wincount++;
+        document.getElementById('result').style.backgroundColor = 'green'
+        document.getElementById('result').style.color = 'black'
     }
     else if (playerChoice === scissors && computerChoice === paper){
-        alert ("You win!")
+        document.getElementById('result').textContent = 'You WON!'
+        wincount++;
+        document.getElementById('result').style.backgroundColor = 'green'
+        document.getElementById('result').style.color = 'black'
     }
     else if (playerChoice === scissors && computerChoice === rock){
-        alert ("You loose!")
+        document.getElementById('result').textContent = 'You Lost!'
+        losecount++;
+        document.getElementById('result').style.backgroundColor = 'red'
+        document.getElementById('result').style.color = 'white'
     }
     else if (playerChoice === rock && computerChoice === scissors){
-        alert ("You win!")
+        document.getElementById('result').textContent = 'You WON!'
+        wincount++;
+        document.getElementById('result').style.backgroundColor = 'green'
+        document.getElementById('result').style.color = 'black'
     }
     else if (playerChoice === paper && computerChoice === scissors){
-        alert ("You loose")
+        document.getElementById('result').textContent = 'You Lost!'
+        losecount++;
+        document.getElementById('result').style.backgroundColor = 'red'
+        document.getElementById('result').style.color = 'white'
     }
+    document.getElementById('computerChoice').textContent = computerChoice
+    document.getElementById('playerChoice').textContent = playerChoice
+    updateScore();
+}
+function updateScore(){
+document.getElementById('wincount').textContent = wincount;
+document.getElementById('losecount').textContent = losecount;
+document.getElementById('tiecount').textContent = tiecount;
+}
+function resetScore(){
+    tiecount = 0;
+    wincount = 0;
+    losecount = 0;
+    updateScore()
+
 }
